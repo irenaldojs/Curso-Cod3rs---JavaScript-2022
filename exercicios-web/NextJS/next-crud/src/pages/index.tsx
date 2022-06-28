f42c92a8daa3b95e7136f3b2c6238c85a0659ed2
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-06-27 22:14:08 -0300

    Crud Next Tabela 3

--- exercicios-web/NextJS/next-crud/src/pages/index.tsx
+++ exercicios-web/NextJS/next-crud/src/pages/index.tsx
@@ -1,20 +1,64 @@
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import { useState } from "react";
 
-const clientes = [
-  new Cliente('Ana', 34, '1'),
-  new Cliente('Bia', 45, '2'),
-  new Cliente('Carlos', 23, '3'),
-  new Cliente('Pedro', 54, '4')
-]
+
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio)
+  const [visivel, setVisivel] = useState<'tabela' | 'formulario'>('tabela')
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente)
+    setVisivel('formulario')
+  }
+  function clienteExcluido(cliente: Cliente) {
+    console.log('Excluir: ' + cliente.nome);
+  }
+  function novoCliente() {
+    setCliente(Cliente.vazio)
+    setVisivel('formulario')
+  }
+  function salvarCliente(cliente: Cliente) {
+    console.log('Salvo: ' + cliente.nome + ' ' + cliente.idade)
+    setVisivel('tabela')
+  }
+
   return (
     <div className='flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 text-white'
     >
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        {visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor='green' className="mb-4"
+                onClick={novoCliente}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}></Tabela>
+          </>
+        ) : (
+          <>
+            <Formulario
+              cliente={cliente}
+              clienteMudou={salvarCliente}
+              cancelado={() => setVisivel('tabela')} />
+
+          </>
+
+        )}
       </Layout>
     </div>
   )
--- exercicios-web/NextJS/next-crud/tailwind.config.js
+++ exercicios-web/NextJS/next-crud/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",    
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-',
+    'to-',
+    'from-',
   ],
   theme: {
     extend: {},
