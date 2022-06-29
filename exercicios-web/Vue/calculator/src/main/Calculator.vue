8800dbcea98064a2b840f2d4358e434641bf5214
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-06-29 19:16:58 -0300

    Vue Js - Calculadora - Componente Button 2

--- exercicios-web/Vue/calculator/src/main/Calculator.vue
+++ exercicios-web/Vue/calculator/src/main/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="calculator">
         <Display value="1000"></Display>
-        <Button label="Ac" triple></Button>
-        <Button label="/" operation></Button>
-        <Button label="7" ></Button>
-        <Button label="8" ></Button>
-        <Button label="9" ></Button>
-        <Button label="*" operation></Button>
-        <Button label="4" ></Button>
-        <Button label="5" ></Button>
-        <Button label="6" ></Button>
-        <Button label="-" operation></Button>
-        <Button label="1" ></Button>
-        <Button label="2" ></Button>
-        <Button label="3" ></Button>
-        <Button label="+" operation></Button>
-        <Button label="0" double></Button>
-        <Button label="." ></Button>
-        <Button label="=" operation></Button>
+        <Button label="Ac" triple @onClick="clearMemory"></Button>
+        <Button label="/" operation @onClick="setOperation"></Button>
+        <Button label="7" @onClick="addDigit"></Button>
+        <Button label="8" @onClick="addDigit"></Button>
+        <Button label="9" @onClick="addDigit"></Button>
+        <Button label="*" operation @onClick="setOperation"></Button>
+        <Button label="4" @onClick="addDigit"></Button>
+        <Button label="5" @onClick="addDigit"></Button>
+        <Button label="6" @onClick="addDigit"></Button>
+        <Button label="-" operation @onClick="setOperation"></Button>
+        <Button label="1" @onClick="addDigit"></Button>
+        <Button label="2" @onClick="addDigit"></Button>
+        <Button label="3" @onClick="addDigit"></Button>
+        <Button label="+" operation @onClick="setOperation"></Button>
+        <Button label="0" double @onClick="addDigit"></Button>
+        <Button label="." @onClick="addDigit"></Button>
+        <Button label="=" operation @onClick="setOperation"></Button>
     </div>
 </template>
 
@@ -27,7 +27,18 @@ import Display from '../components/Display.vue'
 
 export default {
     name: "CalculatorItem",
-    components: { Button, Display }
+    components: { Button, Display },
+    methods: {
+        clearMemory() {
+
+        },
+        setOperation(operation) {
+            
+        },
+        addDigit(n) {
+            
+        }
+    }
 }
 </script>
 
