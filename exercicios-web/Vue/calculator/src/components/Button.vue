34a020b45543c4ba0a8bec95212e3212192600de
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-06-28 23:17:00 -0300

    Vue Js - Calculadora - Criando Componentes

---
+++ exercicios-web/Vue/calculator/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="button">
+        0
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'ButtonItem'
+}
+</script>
+
+<style>
+</style>
---
+++ exercicios-web/Vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="calculator">
+        <Button></Button>
+        <Display></Display>
+    </div>
+</template>
+
+<script>
+import Button from '../components/Button.vue'
+import Display from '../components/Display.vue'
+
+export default {
+    name: "CalculatorItem",
+    components: { Button, Display }
+}
+</script>
+
+<style>
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-auto-columns: repeat(4, 25%);
+    grid-auto-rows: 1fr 48px 48px 48px 48px;
+}
+</style>
