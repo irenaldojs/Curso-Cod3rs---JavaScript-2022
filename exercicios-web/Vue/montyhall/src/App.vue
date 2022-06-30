f14163afb395bdebff6b13b3dcbd2ddd9d1fc616
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-06-29 21:28:05 -0300

    Vue Js - Problema Monty Hall #02

--- exercicios-web/Vue/montyhall/src/App.vue
+++ exercicios-web/Vue/montyhall/src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+        <Gift />
     </div>
 </template>
 
 <script>
+import Gift from './components/Gift.vue'
+
 export default {
+    name: 'AppItem',
+    components: { Gift }
 
 }
 </script>
@@ -21,13 +26,13 @@ body {
     background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
 }
 
-#app{
+#app {
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-#app h1{
+#app h1 {
     border: 1px solid #000;
     background-color: #0004;
     padding: 20px;
