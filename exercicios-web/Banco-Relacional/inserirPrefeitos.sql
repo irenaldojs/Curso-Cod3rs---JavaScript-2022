54016a191a98f5f2e85b85464cda1efe6176e073
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-05 21:58:32 -0300

    10# Banco Relacional - Inserir Prefeitos

---
+++ exercicios-web/Banco-Relacional/inserirPrefeitos.sql
@@ -0,0 +1,16 @@
+insert into prefeitos
+    (nome, cidade_id)
+values
+    ('Rodrigo Neves', 11),
+    ('Raquel Lira', 12),
+    ('Zenaldo Coutinho', null);
+
+insert into `prefeitos`
+    (nome, cidade_id)
+values
+    ('Rafael Greca', null);
+
+insert into `prefeitos`
+    (nome, cidade_id)
+values
+    ('Rodrigo Pinheiro', 12);
