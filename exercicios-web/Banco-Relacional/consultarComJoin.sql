567f28f6babcab3bebaad7de5af8de62507ae42e
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-05 21:15:27 -0300

    9# Banco Relacional - Colsultar Múltiplas Tabelas (JOIN)

---
+++ exercicios-web/Banco-Relacional/consultarComJoin.sql
@@ -0,0 +1,9 @@
+select c.nome as 'Cidade', e.nome as 'Estado', regiao as 'Região' from estados e, cidades c
+    WHERE e.id = c.estado_id;
+
+select
+    c.nome as Cidade,
+    e.nome as Estado,
+    regiao as Região
+from estados e
+inner join cidades c on e.id = c.estado_id
