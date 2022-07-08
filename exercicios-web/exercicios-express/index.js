3030579979f007a49440e9c2e9f622ae4b014251
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-07 23:30:48 -0300

    3# Express - Tipos de Resposta

--- exercicios-web/exercicios-express/index.js
+++ exercicios-web/exercicios-express/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.all('/opa', (req, res) => {
-    res.send('Estou <b>bem</>!')
+app.get('/opa', (req, res) => {
+    //res.send(`<h1>Estou Bem!</h1><br><br><h2>Tipo é HTML!</h2>`)
+
+    //res.json({
+    //    name: 'iPad 32gb',
+    //    price: 1899.00,
+    //    discount: 0.12,
+    //})
+
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200,
+        
+    })
+
 })
 
 app.listen(3000, () => {
